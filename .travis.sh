06974ce63d7e98d4f98fa26ee1c2f0d504283fe9
Amir Chaudhry <amirmc@gmail.com>
2014-03-02 18:20:30 +0000

    add travis scripts
    these are based on avsm's scripts from his post and those I saw on
    the mirage-www and ocaml-uri repos

---
+++ .travis.sh
@@ -0,0 +1,32 @@
+# Edited from https://gist.github.com/avsm/6757425
+# (see post at: http://anil.recoil.org/2013/09/30/travis-and-ocaml.html)
+
+OPAM_DEPENDS="mirage"
+
+case "$OCAML_VERSION,$OPAM_VERSION" in
+4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
+*) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
+esac
+
+# set up machine
+echo "yes" | sudo add-apt-repository ppa:$ppa
+sudo apt-get update -qq
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+export OPAMYES=1
+export OPAMVERBOSE=1
+opam init
+opam install ${OPAM_DEPENDS}
+eval `opam config env`
+
+# Print info on system
+echo OCaml version
+ocaml -version
+echo OPAM versions
+opam --version
+opam --git-version
+echo Mirage version
+mirage --version
+
+# run the commands to build from here
+mirage configure --unix
+mirage build
